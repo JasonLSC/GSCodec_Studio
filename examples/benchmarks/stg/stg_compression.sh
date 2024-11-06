0dbbf33bc566e9ced74ce6f123bdd53c142e3058
Sicheng Li <jasonlisicheng@163.com>
2024-11-06 14:06:10 +0800

    misc: update bash scripts for exp., add py scripts to summary results

--- examples/benchmarks/stg/stg_compression.sh
+++ examples/benchmarks/stg/stg_compression.sh
@@ -1,21 +1,21 @@
+# post-processing compression
 SCENE_DIR="data/neural_3d"
-SCENE_LIST="coffee_martini cook_spinach cut_roasted_beef flame_salmon_1 flame_steak sear_steak"
-# SCENE_LIST="cook_spinach"
-# SCENE_LIST="coffee_martini cut_roasted_beef flame_salmon_1 flame_steak sear_steak"
+SCENE_LIST="cook_spinach cut_roasted_beef flame_salmon_1 flame_steak sear_steak"
+# coffee_martini
 
-RESULT_DIR="results/stg_neu3d_rerun"
+RESULT_DIR="results/stg_neu3d_post_compression"
 
-NUM_FRAME=50
+NUM_FRAME=300
 
 for SCENE in $SCENE_LIST;
 do
     echo "Running $SCENE"
 
-    # CUDA_VISIBLE_DEVICES=1 python simple_trainer_STG.py default \
-    #     --model_path $RESULT_DIR/$SCENE/ \
-    #     --data_dir $SCENE_DIR/$SCENE/colmap_0 \
-    #     --result_dir $RESULT_DIR/$SCENE/ \
-    #     --duration $NUM_FRAME
+    CUDA_VISIBLE_DEVICES=1 python simple_trainer_STG.py default \
+        --model_path $RESULT_DIR/$SCENE/ \
+        --data_dir $SCENE_DIR/$SCENE/colmap_0 \
+        --result_dir $RESULT_DIR/$SCENE/ \
+        --duration $NUM_FRAME
     
     CUDA_VISIBLE_DEVICES=1 python simple_trainer_STG.py default \
         --model_path $RESULT_DIR/$SCENE/ \
@@ -24,7 +24,7 @@ do
         --duration $NUM_FRAME \
         --lpips_net vgg \
         --compression stg \
-        --ckpt $RESULT_DIR/$SCENE/ckpts/ckpt_29999_rank0.pt
+        --ckpt $RESULT_DIR/$SCENE/ckpts/ckpt_best_rank0.pt
 done 
 
 # Zip the compressed files and summarize the stats
---
+++ examples/benchmarks/stg/stg_compression_sim.sh
@@ -0,0 +1,36 @@
+SCENE_DIR="data/neural_3d"
+SCENE_LIST="cut_roasted_beef flame_salmon_1 flame_steak sear_steak"
+# coffee_martini cook_spinach 
+
+RESULT_DIR="results/stg_neu3d_compression_sim"
+
+NUM_FRAME=300
+
+for SCENE in $SCENE_LIST;
+do
+    echo "Running $SCENE"
+
+    CUDA_VISIBLE_DEVICES=1 python simple_trainer_STG.py compression_sim \
+        --model_path $RESULT_DIR/$SCENE/ \
+        --data_dir $SCENE_DIR/$SCENE/colmap_0 \
+        --result_dir $RESULT_DIR/$SCENE/ \
+        --duration $NUM_FRAME \
+    
+    CUDA_VISIBLE_DEVICES=1 python simple_trainer_STG.py compression_sim \
+        --model_path $RESULT_DIR/$SCENE/ \
+        --data_dir $SCENE_DIR/$SCENE/colmap_0 \
+        --result_dir $RESULT_DIR/$SCENE/ \
+        --duration $NUM_FRAME \
+        --lpips_net vgg \
+        --compression stg \
+        --ckpt $RESULT_DIR/$SCENE/ckpts/ckpt_best_rank0.pt
+done 
+
+# Zip the compressed files and summarize the stats
+# if command -v zip &> /dev/null
+# then
+#     echo "Zipping results"
+#     python benchmarks/stg/summarize_stats.py --results_dir $RESULT_DIR --scenes $SCENE_LIST --num_frame $NUM_FRAME
+# else
+#     echo "zip command not found, skipping zipping"
+# fi
