7d324fb1e922404ec63f1ad569393b20ea5c8b24
unknown <zju_chengzhenwu@163.com>
2024-11-06 13:35:41 +0800

    add helper files, including neccessary functions for our representation and Dataset loader for N3D from STG

---
+++ examples/helper/SOM/helper_model.py
@@ -0,0 +1,18 @@
+from typing import Literal
+
+import roma
+import torch
+import torch.nn.functional as F
+
+def cont_6d_to_rmat(cont_6d):
+    """
+    :param 6d vector (*, 6)
+    :returns matrix (*, 3, 3)
+    """
+    x1 = cont_6d[..., 0:3]
+    y1 = cont_6d[..., 3:6]
+    x = F.normalize(x1, dim=-1)
+    y = F.normalize(y1 - (y1 * x).sum(dim=-1, keepdim=True) * x, dim=-1)
+    z = torch.linalg.cross(x, y, dim=-1)
+    
+    return torch.stack([x, y, z], dim=-1)
